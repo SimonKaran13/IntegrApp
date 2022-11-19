fb7570bc3a33aa2dd1834af688a3f6d5ef977644
Simon <simon.karan@tum.de>
2022-11-19 17:51:36 +0100

    fix auth

--- server/src/auth.py
+++ server/src/auth.py
@@ -13,9 +13,10 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 def register():
     if request.method == 'POST':
         username = request.form['username']
+        real_firstname = request.form['first name']
+        real_lastname = request.form['last name']
+        is_resident = 0 if request.form['is_refugee'] == "Yes" else 1
         password = request.form['password']
-        real_firstname = request.form['real_firstname']
-        real_lastname = request.form['real_lastname']
         db = get_db()
         error = None
 
@@ -26,13 +27,15 @@ def register():
         elif not real_firstname:
             error = 'First name is required'
         elif not real_lastname:
-            error = 'Last name is required'    
+            error = 'Last name is required'
+        elif not is_resident:
+            error = 'It is required to know if user is resident or refugee'  
 
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password, real_firstname, real_lastname) VALUES (?, ?, ?, ?)",
-                    (username, generate_password_hash(password), real_firstname, real_lastname),
+                    "INSERT INTO user (username, password, real_firstname, real_lastname, is_resident) VALUES (?, ?, ?, ?, ?)",
+                    (username, generate_password_hash(password), real_firstname, real_lastname, is_resident),
                 )
                 db.commit()
             except db.IntegrityError:
@@ -66,6 +69,7 @@ def login():
             return redirect(url_for('index'))
 
         flash(error)
+    return render_template('auth/login.html')
 
 @bp.before_app_request
 def load_logged_in_user():
